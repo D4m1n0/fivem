d664ba27a6133868f290b9e791eaab9c5e107f3a
Damien <cremer.damien@gmail.com>
2021-09-01 20:58:58 +0200

    Add doorLocked

--- resources/[Items]/Cars/client/main.lua
+++ resources/[Items]/Cars/client/main.lua
@@ -14,3 +14,35 @@ AddEventHandler('vehicle:spawnVehicle', function(vehicle_model)
     SetEntityAsNoLongerNeeded(vehicle)
     SetModelAsNoLongerNeeded(vehicleName)
 end)
+
+Citizen.CreateThread(function()
+  while true do
+    Citizen.Wait(60)
+    local vehicle = GetClosestVehicle(GetEntityCoords(PlayerPedId()), 15.0, 0, 70)
+    print("vehicle", vehicle)
+    if vehicle then
+      if DoesEntityExist(vehicle) then
+        DisplayHelpText("Appuyer sur L")
+        if IsControlPressed(1, 182) then
+          -- check si plaque du véhicule est dans le trousseau de clé du joueur
+          SetVehicleDoorsLocked(vehicle, 1)
+          if GetVehicleDoorsLockedForPlayer(vehicle, PlayerId()) then
+            SetVehicleDoorsLockedForPlayer(vehicle, PlayerId(), false)
+            SetVehicleDoorsLockedForAllPlayers(vehicle, false)
+            notify("Fermeture de la voiture")
+          else
+            SetVehicleDoorsLockedForPlayer(vehicle, PlayerId(), true)
+            SetVehicleDoorsLockedForAllPlayers(vehicle, true)
+            notify("Ouverture de la voiture")
+          end
+        end
+      end
+    end
+  end
+end)
+
+function notify(message)
+  SetNotificationTextEntry("STRING")
+  AddTextComponentString(message)
+  DrawNotification(true, true)
+end
