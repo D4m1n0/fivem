21f99ba6d5df6f26e9b5e415578341d99f46f4f0
Damien <cremer.damien@gmail.com>
2021-09-02 01:32:07 +0200

    Add spawn manager

--- resources/[Player]/Spawn/client/main.lua
+++ resources/[Player]/Spawn/client/main.lua
@@ -1,15 +1,37 @@
-RegisterNetEvent("output")
-AddEventHandler("output", function(argument)
-  TriggerEvent("chatMessage", "[Success]", {0,255,0}, argument)
-end)
-
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
-
 		if NetworkIsSessionStarted() then
-			TriggerServerEvent('spawn:firstPop')
+      print("logging")
+			TriggerServerEvent('spawn:getPlayerSpawn')
 			return
 		end
 	end
+end)
+
+RegisterNetEvent('spawn:spawnPlayer')
+AddEventHandler('spawn:spawnPlayer', function(result)
+  exports.spawnmanager:setAutoSpawnCallback(function()
+      exports.spawnmanager:spawnPlayer({
+          x = result.response.data.x,
+          y = result.response.data.y,
+          z = result.response.data.z,
+          model = 'a_m_m_skater_01'
+      }, function()
+          TriggerEvent('spawn:setMessage', {response={message="Spawn"}})
+      end)
+  end)
+
+  -- enable auto-spawn
+  exports.spawnmanager:setAutoSpawn(true)
+
+  -- and force respawn when the game type starts
+  exports.spawnmanager:forceRespawn()
+end)
+
+RegisterNetEvent('spawn:setMessage')
+AddEventHandler('spawn:setMessage', function(result)
+  SetNotificationTextEntry("STRING")
+  AddTextComponentString(result.response.message)
+  DrawNotification(true, true)
 end)
--- resources/[Player]/Spawn/server/main.lua
+++ resources/[Player]/Spawn/server/main.lua
@@ -1,46 +1,57 @@
--- require "resources/HungerAndThirst/main"
+-- Spawn player somewhere
+-- Check if first spawn
+-- si first spawn alors insert db
+-- sinon recup de la position et spawn à l'endroit
 
-RegisterServerEvent('spawn:firstPop')
-AddEventHandler('spawn:firstPop', function()
+json = require "json"
+
+RegisterServerEvent('spawn:getPlayerSpawn')
+AddEventHandler('spawn:getPlayerSpawn', function()
   local playerID = GetPlayerIdentifiers(source)[1]
+  print('server')
   MySQL.Async.fetchAll("SELECT * FROM users WHERE player_id = @playerID ORDER BY id DESC",
     {["@playerID"] = playerID},
     function(result)
       if result[1] == nil then
-        MySQL.Async.fetchAll("INSERT INTO users (player_id, is_first_connection) VALUES(@playerID, 1)",
-        {["@playerID"] = playerID},
-        function(result)
-          -- TODO setup character_creation script
-        end)
+        firstSpawn()
       else
-        -- TODO get last_position / personal_weapons / skin
+        print('server', json.encode(result))
+        local data = {
+          x= result[1].last_position.x,
+          y= result[1].last_position.y,
+          z= result[1].last_position.z,
+        }
+        TriggerClientEvent('spawn:spawnPlayer', -1, {response={message="Spawn", data=data}})
       end
-end)
-
-
-RegisterServerEvent('spawn:playerSpawn')
-AddEventHandler('spawn:playerSpawn', function(test)
-  print("test", test)
-end)
-
-RegisterCommand("save", function(source, args)
-  MySQL.Async.fetchAll("SELECT * FROM users WHERE player_id = @playerID ORDER BY id DESC",
-  {["@playerID"] = GetPlayerIdentifiers(source)[1]},
-  function(result)
-    if result[1] == nil then
-      MySQL.Async.fetchAll("INSERT INTO users (player_id) VALUES(@playerID)",
-      {["@playerID"] = GetPlayerIdentifiers(source)[1]},
-      function(result)
-        print("test", result)
-      end)
-    else
-      local health = {}
-
-      MySQL.Async.fetchAll("UPDATE users SET health=@health WHERE player_id=@playerID",
-      {["@health"] = json.encode(health), ["@playerID"] = GetPlayerIdentifiers(source)[1]},
-      function(result)
-        print("test", result)
-      end)
-    end
   end)
 end)
+
+function firstSpawn()
+  local location = {
+    x = 1464.723,
+    y = 1105.405,
+    z = 114.3344
+  }
+
+  MySQL.Async.insert("INSERT INTO users (player_id, is_first_connection, last_position) VALUES(@playerID, 1, @location)",
+  {
+    ["@playerID"] = playerID,
+    ["@location"] = json.encode(location),
+  },
+  function(result)
+    TriggerClientEvent('spawn:spawnPlayer', -1, {response={message="Spawn", data=location}})
+  end)
+end
+
+RegisterServerEvent('spawn:setPlayerSpawnUpdate')
+AddEventHandler('spawn:setPlayerSpawnUpdate', function(location)
+  updatePosition(location)
+end)
+
+function updatePosition(location)
+  MySQL.Async.fetchAll("UPDATE users SET health=@health WHERE player_id=@playerID",
+  {["@last_position"] = json.encode(location), ["@playerID"] = GetPlayerIdentifiers(source)[1]},
+  function(result)
+    TriggerClientEvent('spawn:setMessage', -1, {response={message="Update position"}})
+  end)
+end
