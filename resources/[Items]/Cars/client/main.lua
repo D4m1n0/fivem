a1ae0578fc61ee8556039cf8a6abb8d6c1cb222b
Damien <cremer.damien@gmail.com>
2021-09-02 01:31:38 +0200

    Add lock car into car

--- resources/[Items]/Cars/client/main.lua
+++ resources/[Items]/Cars/client/main.lua
@@ -15,34 +15,60 @@ AddEventHandler('vehicle:spawnVehicle', function(vehicle_model)
     SetModelAsNoLongerNeeded(vehicleName)
 end)
 
+local controlWhile = 0
+
 Citizen.CreateThread(function()
   while true do
-    Citizen.Wait(60)
-    local vehicle = GetClosestVehicle(GetEntityCoords(PlayerPedId()), 15.0, 0, 70)
-    print("vehicle", vehicle)
+    Citizen.Wait(10)
+    local vehicle = GetClosestVehicle(GetEntityCoords(PlayerPedId()), 1.5, 0, 70)
+
+    if IsPedSittingInVehicle(PlayerPedId(), vehicle) and GetPedInVehicleSeat(vehicle, -1) == GetPlayerPed(PlayerPedId()) then
+      DisplayHelpText("Appuyer sur G")
+      if IsControlPressed(1, 183) and controlWhile == 0 then
+        -- check si plaque du véhicule est dans le trousseau de clé du joueur
+        controlWhile = 1
+        vehicleDoorsLocked(vehicle)
+      end
+    end
+
     if vehicle then
       if DoesEntityExist(vehicle) then
-        DisplayHelpText("Appuyer sur L")
-        if IsControlPressed(1, 182) then
+        DisplayHelpText("Appuyer sur G")
+        if IsControlPressed(1, 183) and controlWhile == 0 then
           -- check si plaque du véhicule est dans le trousseau de clé du joueur
-          SetVehicleDoorsLocked(vehicle, 1)
-          if GetVehicleDoorsLockedForPlayer(vehicle, PlayerId()) then
-            SetVehicleDoorsLockedForPlayer(vehicle, PlayerId(), false)
-            SetVehicleDoorsLockedForAllPlayers(vehicle, false)
-            notify("Fermeture de la voiture")
-          else
-            SetVehicleDoorsLockedForPlayer(vehicle, PlayerId(), true)
-            SetVehicleDoorsLockedForAllPlayers(vehicle, true)
-            notify("Ouverture de la voiture")
-          end
+          controlWhile = 1
+          vehicleDoorsLocked(vehicle)
         end
       end
     end
   end
 end)
 
+function vehicleDoorsLocked(vehicle)
+  SetVehicleDoorsLocked(vehicle, 1)
+  if GetVehicleDoorsLockedForPlayer(vehicle, PlayerId()) then
+    SetVehicleDoorsLockedForPlayer(vehicle, PlayerId(), false)
+    SetVehicleDoorsLockedForAllPlayers(vehicle, false)
+    notify("Ouverture de la voiture")
+    Wait(60)
+    controlWhile = 0
+  else
+    SetVehicleDoorsLockedForPlayer(vehicle, PlayerId(), true)
+    SetVehicleDoorsLockedForAllPlayers(vehicle, true)
+    notify("Fermeture de la voiture")
+    Wait(60)
+    controlWhile = 0
+  end
+end
+
 function notify(message)
   SetNotificationTextEntry("STRING")
   AddTextComponentString(message)
   DrawNotification(true, true)
+end
+
+function DisplayHelpText(str)
+    SetTextComponentFormat("STRING")
+    AddTextComponentString(str)
+    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
 end
